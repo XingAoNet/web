f75d064db40f3a22e1a265a39497308948df4a9d
klicevip <klice@qq.com>
2015-02-16 13:47:50 +0800

    添加注释

--- Core/XingAo.Core/Result.cs
+++ Core/XingAo.Core/Result.cs
@@ -6,10 +6,22 @@ using System.Collections;
 
 namespace XingAo.Core
 {
+    /// <summary>
+    /// 接口返回对象
+    /// </summary>
     public class Result
     {
+        /// <summary>
+        /// 结果代码，约定返回0表示成功
+        /// </summary>
         public int ResultNo { get; set; }
+        /// <summary>
+        /// 结果描述
+        /// </summary>
         public string ResultDescription { get; set; }
+        /// <summary>
+        /// 结果其他数据
+        /// </summary>
         public Hashtable ResultData { get; set; }
     }
 }
